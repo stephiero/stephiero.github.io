00c6ae35555a7bf29fa3c0fe6249faea5f8821f8
Stephanie <stephanierojas608@gmail.com>
2022-06-08 14:46:09 -0400

    nav3

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                //link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
                 link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 }s';
 
                 console.log(index / 7);
             }
         });
+
+        burger.classList.toggle('toggle');
     });
 
 }
